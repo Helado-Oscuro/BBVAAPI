5e1da57942489de95fcd9b6107570b128fb2876f
Enzo Carrión <enz10.cr@gmail.com>
2022-08-27 15:04:45 -0500

    Editando entidades

--- Models/Office.cs
+++ Models/Office.cs
@@ -1,17 +1,21 @@
-﻿namespace BBVA.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BBVA.Models
 {
     public class Office
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public int VentanillasTotalQuantity { get; set; }
         public int PlataformasTotalQuantity { get; set; }
         public string Latitude { get; set; }
-        public string Length { get; set; }
+        public string Longitude { get; set; }
         public int Capacity { get; set; }
         public DateTime CheckInTime { get; set; }
         public DateTime CheckOutTime { get; set; }
-        public DateTime Place { get; set; }
-
-
+        public String Place { get; set; }
     }
 }
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,12 +1,18 @@
-﻿namespace BBVA.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BBVA.Models
 {
     public class Ticket
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public int State { get; set; }
         public DateTime CreatedTime { get; set; }
         public DateTime UpdatedTime { get; set; }
-        public User User { get; set; }
-        public Office Office { get; set; }
+        [ForeignKey("IdUser")] public virtual User User { get; set; }
+        [ForeignKey("IdAttentionChannel")] public virtual AttentionChannel AttentionChannel { get; set; }
     }
 }
